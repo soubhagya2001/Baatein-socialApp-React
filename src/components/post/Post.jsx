dfae3df6a66e678c1de839079eb81033a0656e86
soubhagya2001 <soubhagyaprusty36@gmail.com>
2024-03-09 18:26:20 +0530

    New Commit

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,32 +1,34 @@
 import "./post.css"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Users } from "../../dummyData";
 
-export default function Post() {
+export default function Post({post}) {
+    
   return (
     <div className="post">
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img src="/assets/people/1.jpg" alt="" className="postProfileImg" />
-                    <span className="postUsername">Saiyana Sa</span>
-                    <span className="postDate">5 mins ago</span>
+                    <img src={Users.filter((u)=>u.id===post.userId)[0].profilePicture} alt="" className="postProfileImg" />
+                    <span className="postUsername">{Users.filter((u)=>u.id===post.userId)[0].username}</span>
+                    <span className="postDate">{post.date}</span>
                 </div>
                 <div className="postTopRight">
                     <MoreVertIcon />
                 </div>
             </div>
             <div className="postCenter">
-                <span className="postText">Hey! How are You ?</span>
-                <img src="assets/posts/1.jpg" className="postImg" alt="" />
+                <span className="postText">{post?.desc}</span>
+                <img src={post.photo} className="postImg" alt="" />
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
                     <img className="likeIcon" src="assets/like.png" alt="" />
                     <img className="likeIcon" src="assets/heart.png" alt="" />
-                    <div className="postLikeCounter">22 people liked</div>
+                    <div className="postLikeCounter">{post.like} people liked</div>
                 </div>
                 <div className="postBottomRight">
-                    <div className="postCommentText">9 comments</div>
+                    <div className="postCommentText">{post.comment} comments</div>
                 </div>
             </div>
         </div>
--- src/dummyData.js
+++ src/dummyData.js
@@ -1,33 +1,178 @@
 export const Users = [
-    {
-        id:1,
-        profilePicture : "assets/people/1.jpg",
-        username: "Shakila Sa",
-    },
+  {
+    id: 1,
+    profilePicture: "assets/people/1.jpg",
+    username: "Shakila Sa",
+  },
 
-    {
-        id:2,
-        profilePicture : "assets/people/2.jpg",
-        username: "Krishna Sharma",
-    },
+  {
+    id: 2,
+    profilePicture: "assets/people/2.jpg",
+    username: "Krishna Sharma",
+  },
 
-    {
-        id:3,
-        profilePicture : "assets/people/3.jpg",
-        username: "Gautami Sahu",
-    },
+  {
+    id: 3,
+    profilePicture: "assets/people/3.jpg",
+    username: "Gautami Sahu",
+  },
 
-    {
-        id:4,
-        profilePicture : "assets/people/4.jpg",
-        username: "Ankit bhatt",
-    },
+  {
+    id: 4,
+    profilePicture: "assets/people/4.jpg",
+    username: "Ankit bhatt",
+  },
 
-    {
-        id:5,
-        profilePicture : "assets/people/5.jpg",
-        username: "Shanu goyal",
-    }
+  {
+    id: 5,
+    profilePicture: "assets/people/5.jpg",
+    username: "Shanu goyal",
+  },
+
+  {
+    id: 6,
+    profilePicture: "assets/people/6.jpg",
+    username: "Rahul Singh",
+  },
+
+  {
+    id: 7,
+    profilePicture: "assets/people/7.jpg",
+    username: "Priya Patel",
+  },
+
+  {
+    id: 8,
+    profilePicture: "assets/people/8.jpg",
+    username: "Aryan Kapoor",
+  },
+
+  {
+    id: 9,
+    profilePicture: "assets/people/9.jpg",
+    username: "Neha Sharma",
+  },
+
+  {
+    id: 10,
+    profilePicture: "assets/people/10.jpg",
+    username: "Vivek Verma",
+  },
+
+  {
+    id: 11,
+    profilePicture: "assets/people/11.jpg",
+    username: "Kavya Singh",
+  },
 ];
 
+export const Posts = [
+  {
+    id: 1,
+    desc: "Love all , Hate none",
+    photo: "assets/posts/1.jpg",
+    date: "5 mins ago",
+    userId: 1,
+    like: 29,
+    comment: 9,
+  },
+  {
+    id: 2,
+    desc: "Spread kindness everywhere you go",
+    photo: "assets/posts/2.jpg",
+    date: "10 mins ago",
+    userId: 2,
+    like: 42,
+    comment: 7,
+  },
 
+  {
+    id: 3,
+    desc: "Embrace the beauty of simplicity",
+    photo: "assets/posts/3.jpg",
+    date: "15 mins ago",
+    userId: 3,
+    like: 18,
+    comment: 3,
+  },
+
+  {
+    id: 4,
+    desc: "Find joy in the little things",
+    photo: "assets/posts/4.jpg",
+    date: "20 mins ago",
+    userId: 4,
+    like: 36,
+    comment: 11,
+  },
+
+  {
+    id: 5,
+    desc: "Smile often, it's contagious",
+    photo: "assets/posts/5.jpg",
+    date: "25 mins ago",
+    userId: 5,
+    like: 25,
+    comment: 6,
+  },
+
+  {
+    id: 6,
+    desc: "Be the reason someone believes in goodness",
+    photo: "assets/posts/6.jpg",
+    date: "30 mins ago",
+    userId: 6,
+    like: 33,
+    comment: 8,
+  },
+
+  {
+    id: 7,
+    desc: "Spread love like confetti",
+    photo: "assets/posts/7.jpg",
+    date: "35 mins ago",
+    userId: 7,
+    like: 39,
+    comment: 10,
+  },
+
+  {
+    id: 8,
+    desc: "Kindness is free, sprinkle it everywhere",
+    photo: "assets/posts/8.jpg",
+    date: "40 mins ago",
+    userId: 8,
+    like: 21,
+    comment: 4,
+  },
+
+  {
+    id: 9,
+    desc: "Make today so awesome, yesterday gets jealous",
+    photo: "assets/posts/9.jpg",
+    date: "45 mins ago",
+    userId: 9,
+    like: 28,
+    comment: 5,
+  },
+
+  {
+    id: 10,
+    desc: "Life is short, choose happiness",
+    photo: "assets/posts/10.jpg",
+    date: "50 mins ago",
+    userId: 10,
+    like: 37,
+    comment: 12,
+  },
+
+  {
+    id: 11,
+    desc: "Be the light in someone's darkness",
+    photo: "assets/posts/11.jpg",
+    date: "55 mins ago",
+    userId: 11,
+    like: 45,
+    comment: 13,
+  },
+];
